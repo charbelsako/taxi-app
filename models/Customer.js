90e1cb207048af1e9cdc6b1e268649749737d113
Charbel Sarkis <charbelsako@gmail.com>
2024-07-26 19:06:59 +0300

    adds timestamps to customers

--- models/Customer.js
+++ models/Customer.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const { autoIncrement } = require('mongoose-plugin-autoinc');
 
-const customerSchema = new mongoose.Schema({
-  phone: { type: String },
-  name: { type: String },
-  address: { type: String },
-});
+const customerSchema = new mongoose.Schema(
+  {
+    phone: { type: String },
+    name: { type: String },
+    address: { type: String },
+  },
+  { timestamps: true }
+);
 
 customerSchema.plugin(autoIncrement, {
   model: 'customer',
