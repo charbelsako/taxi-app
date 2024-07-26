4b9c0cef8c6ef6085a9cea95a0fa462b1e925794
Charbel Sarkis <charbelsako@gmail.com>
2024-07-26 19:23:34 +0300

    adds code to view frontend. removes cors allowed origins since its not needed (same origin)

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,12 +2,11 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else if (process.env.NODE_ENV === 'development' && !origin) {
       callback(null, true);
     } else {
-	 console.log(origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRouter = require('./routes/user');
 const customerRouter = require('./routes/customer');
 
 const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+// const corsOptions = require('./config/corsOptions');
 const credentials = require('./middleware/credentials');
 
 const mongoURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@gymappcluster.rs63kkd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -29,12 +29,22 @@ const app = express();
 
 app.use(cookieParser());
 app.use(credentials);
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/auth/', authRouter);
 app.use('/api/v1/user/', userRouter);
 app.use('/api/v1/customer/', customerRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('production mode');
+  app.use(express.static('client/build'));
+  // console.log(path.resolve(__dirname, "client/", "build/", "index.html"))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/', 'build/', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
