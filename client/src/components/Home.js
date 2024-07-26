64782b2c8ba9b6b07ff4d90e3330c10a1a07c65d
Charbel Sarkis <charbelsako@gmail.com>
2024-07-26 17:44:28 +0300

    Fixes home to show proper styled inputs

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import TextFieldGroup from './TextFieldGroup';
+import TextAreaFieldGroup from './TextAreaFieldGroup';
 
 const Home = () => {
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='col-12'>
+      <div className=''>
+        <div className='text-6xl'>
           <h1>Home</h1>
+        </div>
+        {/* Phone Input */}
+        <div className='row align-items-center mt-4'>
+          <div className='col-2'>
+            <label htmlFor=''>Phone:</label>
+          </div>
+          <div className='col-6'>
+            <TextFieldGroup placeholder='Enter Phone' />
+          </div>
+          <div className='col-4'>
+            <button className='btn btn-success'>Search</button>
+          </div>
+        </div>
+
+        {/* Name */}
+        <div className='row align-items-center mt-4'>
+          <div className='col-2'>
+            <label htmlFor=''>Name:</label>
+          </div>
+          <div className='col-6'>
+            <TextFieldGroup placeholder='Enter Name' />
+          </div>
+        </div>
+        {/* Address */}
+        <div className='row align-items-center mt-4'>
+          <div className='col-2'>
+            <label htmlFor=''>Address:</label>
+          </div>
+          <div className='col-6'>
+            <TextAreaFieldGroup placeholder='address' />
+          </div>
         </div>
       </div>
     </div>
